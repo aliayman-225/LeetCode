1567eb46f1080fbc535e33d7b95b7d1c5e2248f5
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2024-07-10 13:21:43 +0300

    Time: 5 ms (40.74%), Space: 8.1 MB (29.15%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -14,44 +14,22 @@ class Solution {
     public:
         int findTheWinner(int n, int k) {
             Node* head =  new Node(1);
-            Node* temp = NULL;
+            Node* temp = head;
             for(int i=1;i<n;i++)
             {
                 Node* new_node =  new Node(i+1);
-                if(i==1)
-                {
-                    head->next = new_node;
-                    temp =new_node;
-                }     
-                else if(i==(n))
-                    temp->next=head;
-                else
-                {
-                    temp-> next = new_node;
-                    temp= new_node;
-                }
-
+                temp->next =new_node;  
+                temp= new_node;
             }
             temp-> next = head;
             head->prev=temp;
             temp = head;
-            int skip;
+           
             for(int i=0;i<n;i++)
             {
                 if(k==1)
-                {
                     return head->prev->data;
-                     /*   Node * deletedNode=head;
-                        deletedNode->prev->next=deletedNode->next;
-                        head=deletedNode->next;
-                        head->prev=deletedNode->prev;
-                        skip++;
-                    if((temp->data)==(temp->next->data))
-                        break;*/
-                }
-                    
-                else
-                    skip=1;
+                int skip=1;
                 while(skip!=k)
                 {
                     if(skip==k-1)
