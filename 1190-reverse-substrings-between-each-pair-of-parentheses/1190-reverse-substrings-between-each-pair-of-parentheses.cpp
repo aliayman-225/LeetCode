2d189a3cbdd055c40e27d128a86c6eb653ff33f0
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2024-07-11 23:58:53 +0300

    Time: 8 ms (5.11%), Space: 10.4 MB (13.2%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        string newString=s;
+        string subString="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==')' && subString.length()==0)
+            {
+                for(int j=i;j>=0;j--)
+                {
+                    if(s[j]!='(' &&s[j]!=')')
+                    {
+                        subString+=(s[j]);
+                    }
+                    else if (s[j]!='(' && s[j]==')' && s[j-1]=='(')
+                    {
+                        newString.erase(j-1,2);
+                        subString="()";
+                        break;
+                    }
+                    else if(s[j]!='(' &&s[j]==')')
+                    {
+                       subString+=(s[j-1]); 
+                       j--;
+                    }
+                        
+                    else{
+                        newString.erase(j, subString.length()+2);
+                        newString.insert(j,subString);
+                        cout<<subString<<" "<<newString;
+                        break;
+                    }
+                }
+            }
+            else if(subString.length()!=0)
+                break;
+        }
+        if(newString.find('(')!=string::npos)
+            return reverseParentheses(newString);
+        else
+            return newString;      
+    }
+};
